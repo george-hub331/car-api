bf6a6b017bcd034dabcf5e60a1647bcdb993d1a7
george-hub331 <idiaghegeorge9@gmail.com>
2021-11-05 08:22:02 +0100

    ...

--- class.php
+++ class.php
@@ -34,7 +34,7 @@ class car{
                 if($num <= $last){
                     $longitude = !isset($mm["logitude"]) ? $mm["longitude"] : $mm["logitude"];
                     
-                    $llz .= "<li><strong> $mm[date] - $mm[time] </strong><br><div class='mapouter'><div class='gmap_canvas'><iframe width='400' height='300' id='gmap_canvas' src='https://maps.google.com/maps?q=$mm[latitude],$longitude&t=&z=13&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'></iframe><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div></li><br><br>";
+                    $llz .= "<li><strong> $mm[date] - $mm[time] </strong><br><div class='mapouter'><div class='gmap_canvas'><iframe width='500' height='400' id='gmap_canvas' src='https://maps.google.com/maps?q=$mm[latitude],$longitude&t=&z=13&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'></iframe><style>.mapouter{position:relative;text-align:right;height:400px;width:500px;}</style><style>.gmap_canvas {overflow:hidden;background:none!important;height:400px;width:500px;}</style></div></div></li><br><br>";
                 }
                 
                 $num++;
