54981d3379d54367791ff970f5e0efab220af93c
george-hub331 <idiaghegeorge9@gmail.com>
2021-11-11 16:59:21 +0100

    ...

--- add_location.php
+++ add_location.php
@@ -12,5 +12,6 @@ echo $a->createLoc($_GET['lat']/*latitude*/,$_GET['long']/*longitude*/,$_GET['da
 }else{
     echo "data supplied incomplete";
 }
-echo "</div><a style='width:calc(100% - 16px);text-align:center;padding:10px;margin-bottom:5px;display:block;background-color:#069C54;color:#fff;text-decoration:none;cursor:pointer;' href='addlocation.php?mode=$_GET[id]'>Add Another Location</a>";
+echo "</div><a style='width:calc(100% - 16px);text-align:center;padding:10px;margin-bottom:5px;display:block;background-color:#069C54;color:#fff;text-decoration:none;cursor:pointer;' href='addlocation.php?mode=$_GET[id]'>Add Another Location</a>
+<a style='width:calc(100% - 16px);text-align:center;padding:10px;margin-bottom:5px;display:block;background-color:#069C54;color:#fff;text-decoration:none;cursor:pointer;' href='viewlast.php?mode=$_GET[id]'>View X Location For This Device</a>";
 ?>
---
+++ viewlast.php
@@ -0,0 +1,60 @@
+<!--view last x locations of a car-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styles.css">
+
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>View Last Locations</title>
+</head>
+
+<body class="signup">
+    <div class="l-form">
+        <div class="shape1"></div>
+        <div class="shape2"></div>
+
+        <div class="form">
+
+            <form method="POST" entype="multipart/form-data" action="last_known.php" class="form__content signup">
+                <h1 class="form_title"><span>View Last Locations</span></h1>
+
+                <div class="form__div form__div-one <?php echo isset($_GET['mode']) ? 'focus' : ''; ?>">
+                    <div class="form__icon">
+                        <i class='bx bx-user'></i>
+                    </div>
+
+                    <div class="form__div-input">
+                        <label for="" class="form__label">Unique Id</label>
+                        <input name="id" type="text" <?php echo isset($_GET['mode']) ? 'value="' . $_GET['mode'] . '"' : ''; ?> class="form__input">
+                    </div>
+                </div>
+
+
+                <div class="form__div form__div-one">
+                    <div class="form__icon">
+                        <i class='bx bx-map-pin'></i>
+                    </div>
+
+                    <div class="form__div-input">
+                        <label for="" class="form__label">Last X Locations</label>
+                        <input name="num" type="text" class="form__input">
+                    </div>
+                </div>
+
+
+                <input type="submit" class="form__button" value="Next">
+
+            </form>
+        </div>
+
+    </div>
+
+    <script src="mi.js"></script>
+</body>
+
+</html>
